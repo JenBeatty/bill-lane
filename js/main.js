a42ae209e5fb6ef025df914b7439e6768239e28f
Jennifer Beatty <jbeatty@southlandind.com>
2014-01-27 07:08:03 -0500

    Adding title to the team page and initial code for the team page toggle.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(function(){
+		
+		$('.more').click('on', function(){
+			$('.teamtoggle').slideDown('slow');
+
+		});
+		
+		$('.less').click('on', function(){
+			$('.teamtoggle').slideUp('slow');
+
+		});
+		
+		$('.open').click('on', function(){
+			$('.caroltoggle').slideDown('slow');
+
+		});
+		
+		$('.close').click('on', function(){
+			$('.caroltoggle').slideUp('slow');
+
+		});
+});
+
+
 
