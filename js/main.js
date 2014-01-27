b81f2c41754d5934407a401fd83cb9bbc2ce07a9
Jennifer Beatty <jbeatty@southlandind.com>
2014-01-27 08:30:01 -0500

    Finishing team page toggle and content.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,52 @@
 $(function(){
 		
-		$('.more').click('on', function(){
-			$('.teamtoggle').slideDown('slow');
+		$('.billmore').click('on', function(){
+			$('.billtoggle').slideDown('slow');
 
 		});
 		
-		$('.less').click('on', function(){
-			$('.teamtoggle').slideUp('slow');
+		$('.billless').click('on', function(){
+			$('.billtoggle').slideUp('slow');
 
 		});
 		
-		$('.open').click('on', function(){
+		$('.carolmore').click('on', function(){
 			$('.caroltoggle').slideDown('slow');
 
 		});
 		
-		$('.close').click('on', function(){
+		$('.carolless').click('on', function(){
 			$('.caroltoggle').slideUp('slow');
+
+		});
+		
+		$('.bobmore').click('on', function(){
+			$('.bobtoggle').slideDown('slow');
+
+		});
+		
+		$('.bobless').click('on', function(){
+			$('.bobtoggle').slideUp('slow');
+
+		});
+		
+		$('.johnmore').click('on', function(){
+			$('.johntoggle').slideDown('slow');
+
+		});
+		
+		$('.johnless').click('on', function(){
+			$('.johntoggle').slideUp('slow');
+
+		});
+
+		$('.janetmore').click('on', function(){
+			$('.janettoggle').slideDown('slow');
+
+		});
+		
+		$('.janetless').click('on', function(){
+			$('.janettoggle').slideUp('slow');
 
 		});
 });
