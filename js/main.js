1fb815562638a68718e041170e27c2d67465138a
Jennifer Beatty <jbeatty@southlandind.com>
2014-01-29 09:46:37 -0500

    Updating navigation to have a dropdown sub menu on hover. Updating css. TODO is to get the dropdown in front of the main content.

--- js/main.js
+++ js/main.js
@@ -51,3 +51,9 @@ $(function(){
 		});
 });
 
+	$(function () {
+		$('nav li ul').hide().removeClass('fallback');
+		$('nav li').hover(function () {
+			$('ul', this).stop().slideToggle(200);
+		});
+	});
